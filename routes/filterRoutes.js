9ad2d8afd788edc60a9211927d63490c37f18e42
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2024-04-26 13:51:33 +0800

    Unsure commit

--- routes/filterRoutes.js
+++ routes/filterRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../database/db');
-const {authenticateToken} = require('../authentication/middleware');
 
 const router = express.Router();
 
