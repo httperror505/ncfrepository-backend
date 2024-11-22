b8930dd2a196e5a2cf92a6898efa1ba86e6f0148
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2024-11-23 01:01:11 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    // port: process.env.PORT,
+    port: process.env.PORT,
     // connectTimeout: 10000 // Set the timeout to 10 seconds
 });
 
